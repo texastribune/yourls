67b8338863eb934588b7f8d99eeb4a6ffd2bcae5
Daniel Craigmile <dcraigmile@texastribune.org>
2018-09-13 15:24:35 -0500

    add bulk-shortening script

---
+++ bulk-shorten.py
@@ -0,0 +1,46 @@
+import requests
+import csv
+
+api_url = ""
+user = ""
+password = ""
+
+all = list()
+
+with open("input.csv") as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader, None)
+    for row in reader:
+        title = row[3]
+        url = row[6]
+
+        payload = {
+            "url": url,
+            "format": "json",
+            "title": title,
+            "action": "shorturl",
+            "username": user,
+            "password": password,
+        }
+
+        response = requests.post(api_url, data=payload)
+
+        if response.status_code != 200:
+            print("error")
+            print(url)
+            continue
+
+        response = response.json()
+        if response["status"] != "success":
+            print("error")
+            print(url)
+            continue
+        shorturl = response["shorturl"]
+        all.append({"email": title, "long url": url, "short url": shorturl})
+
+
+with open("output.csv", mode="w") as outfile:
+    fieldnames = ["email", "long url", "short url"]
+    writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(all)
